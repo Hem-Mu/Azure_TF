fc546dc0e164b050903926d5a7478120e88296d7
HemMu <kimmim1223@naver.com>
2021-10-27 20:14:46 +0900

    10-27-20:14

--- 04_subnet.tf
+++ 04_subnet.tf
@@ -4,6 +4,7 @@ resource "azurerm_subnet" "hem-pub1" {
   virtual_network_name = azurerm_virtual_network.hem-vpn.name
   address_prefixes     = ["10.0.0.0/24"]
 }#subnet pub1
+
 resource "azurerm_subnet" "hem-pri1" {
   name                 = "pri1"
   resource_group_name  = azurerm_resource_group.hem_RG.name
