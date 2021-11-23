339c1e6d44c81061bdfc82021a617f3738a010b5
HemMu <kimmim1223@naver.com>
2021-11-23 10:38:02 +0900

    11-23-10:37

--- 010_aks.tf
+++ 010_aks.tf
@@ -5,7 +5,7 @@ resource "azurerm_user_assigned_identity" "identity" {
     name = "identity1"
 }
 
-resource "azurerm_kubernetes_cluster" "hem-aks" {
+resource "azurerm_kubernetes_cluster" "hem-aks"{ 
   name                = "terraform-aks"
   location            = azurerm_resource_group.hem-rg.location
   resource_group_name = azurerm_resource_group.hem-rg.name
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "hem-aks" {
 
   default_node_pool {
     name       = "node"
-    # node_count = 2 # node 갯수
+    # node_count = 2 # node number
     vm_size    = "Standard_D2_v2"
     type       = "VirtualMachineScaleSets"
     enable_auto_scaling = true
@@ -37,111 +37,58 @@ resource "azurerm_kubernetes_cluster" "hem-aks" {
     service_cidr = "10.0.0.0/16"
     docker_bridge_cidr = "172.17.0.1/16"
     dns_service_ip = "10.0.10.0"
+  }
+
+  role_based_access_control {
+    enabled = false
+  }
+
+  addon_profile {
+    ingress_application_gateway {
+      enabled = true
+      gateway_id = azurerm_application_gateway.agw.id
+    } # agw attach
+
+    http_application_routing {
+        enabled = false
     }
+  }
 
   identity {
     type = "SystemAssigned"
-  } # Cluster ID type
-  
-  role_based_access_control {
-    enabled = true
-    }
+  }
 
+#   service_principal {
+#     client_id     = var.aks_service_principal_app_id
+#     client_secret = var.aks_service_principal_client_secret
+#     } # Cluster ID
+  depends_on = [azurerm_virtual_network.hem-vnet, azurerm_application_gateway.agw]
 }
 
-# resource "azurerm_public_ip" "agw-ip" {
-#     name                         = "AGW-ip"
-#     location                     = azurerm_resource_group.hem-rg.location
-#     resource_group_name          = azurerm_resource_group.hem-rg.name
-#     allocation_method            = "Static"
-# } # AGW IP
+resource "azurerm_role_assignment" "ra1" {
+    scope                = azurerm_subnet.hem-pub[0].id
+    role_definition_name = "Network Contributor"
+    principal_id         = var.aks_service_principal_object_id 
+    depends_on = [azurerm_virtual_network.hem-vnet]
+}
 
-# resource "azurerm_application_gateway" "agw" {
-#     name                = "AGW"
-#     resource_group_name = azurerm_resource_group.hem-rg.name
-#     location            = azurerm_resource_group.hem-rg.location
+resource "azurerm_role_assignment" "ra2" {
+    scope                = azurerm_user_assigned_identity.identity.id
+    role_definition_name = "Managed Identity Operator"
+    principal_id         = var.aks_service_principal_object_id
+    depends_on           = [azurerm_user_assigned_identity.identity]
+}
 
-#     sku {
-#     name     = "Standard_v2"
-#     tier     = "Standard_v2"
-#     capacity = 2
-#     }
+resource "azurerm_role_assignment" "ra3" {
+    scope                = azurerm_application_gateway.agw.id
+    role_definition_name = "Contributor"
+    principal_id         = azurerm_user_assigned_identity.identity.principal_id
+    depends_on           = [azurerm_user_assigned_identity.identity, azurerm_application_gateway.agw]
+}
 
-#     gateway_ip_configuration {
-#     name      = "appGatewayIpConfig"
-#     subnet_id = azurerm_subnet.hem-pub[1].id
-#     }
-
-#     frontend_port {
-#     name = "httpPort"
-#     port = 80
-#     }
-
-#     frontend_port {
-#     name = "httpsPort"
-#     port = 443
-#     }
-
-#     frontend_ip_configuration {
-#     name                 = "front_ip"
-#     public_ip_address_id = azurerm_public_ip.agw-ip.id
-#     }
-
-#     backend_address_pool {
-#     name = "backend_pool"
-#     }
-
-#     backend_http_settings {
-#     name                  = "http_setting"
-#     cookie_based_affinity = "Disabled"
-#     port                  = 80
-#     protocol              = "Http"
-#     request_timeout       = 1
-#     }
-
-#     http_listener {
-#     name                           = "listener"
-#     frontend_ip_configuration_name = "frontend_ip_configuration"
-#     frontend_port_name             = "frontend_port"
-#     protocol                       = "Http"
-#     }
-
-#     request_routing_rule {
-#     name                       = "request_routing_rule"
-#     rule_type                  = "Basic"
-#     http_listener_name         = "listener"
-#     backend_address_pool_name  = "backend_address_pool"
-#     backend_http_settings_name = "http_setting"
-#     }
-
-#     depends_on = [azurerm_virtual_network.hem-vnet, azurerm_public_ip.agw-ip]
-# }
-
-# resource "azurerm_role_assignment" "ra1" {
-#     scope                = azurerm_subnet.hem-pub[0].id
-#     role_definition_name = "Network Contributor"
-#     principal_id         = var.aks_service_principal_object_id 
-
-#     depends_on = [azurerm_virtual_network.test]
-# }
-
-# resource "azurerm_role_assignment" "ra2" {
-#     scope                = azurerm_user_assigned_identity.identity.id
-#     role_definition_name = "Managed Identity Operator"
-#     principal_id         = var.aks_service_principal_object_id
-#     depends_on           = [azurerm_user_assigned_identity.identity]
-# }
-
-# resource "azurerm_role_assignment" "ra3" {
-#     scope                = azurerm_application_gateway.agw.id
-#     role_definition_name = "Contributor"
-#     principal_id         = azurerm_user_assigned_identity.identity.principal_id
-#     depends_on           = [azurerm_user_assigned_identity.identity, azurerm_application_gateway.network]
-# }
-
-# resource "azurerm_role_assignment" "ra4" {
-#     scope                = azurerm_resource_group.hem-rg.id
-#     role_definition_name = "Reader"
-#     principal_id         = azurerm_user_assigned_identity.identity.principal_id
-#     depends_on           = [azurerm_user_assigned_identity.identity, azurerm_application_gateway.network]
-# }
+resource "azurerm_role_assignment" "ra4" {
+    scope                = azurerm_resource_group.hem-rg.id
+    role_definition_name = "Reader"
+    principal_id         = azurerm_user_assigned_identity.identity.principal_id
+    depends_on           = [azurerm_user_assigned_identity.identity, azurerm_application_gateway.agw]
+}
---
+++ 09_AGW.tf
@@ -0,0 +1,68 @@
+resource "azurerm_public_ip" "agw-ip" {
+    name                         = "AGW-ip"
+    location                     = azurerm_resource_group.hem-rg.location
+    resource_group_name          = azurerm_resource_group.hem-rg.name
+    allocation_method            = "Static"
+    sku                          = "Standard"
+} # AGW IP
+
+resource "azurerm_application_gateway" "agw" {
+    name                = "AGW"
+    resource_group_name = azurerm_resource_group.hem-rg.name
+    location            = azurerm_resource_group.hem-rg.location
+
+    sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+    }
+
+    gateway_ip_configuration {
+    name      = "appGatewayIpConfig"
+    subnet_id = azurerm_subnet.hem-pub[1].id # frontend subnet
+    }
+
+    frontend_port {
+    name = "httpPort"
+    port = 80
+    }
+
+    frontend_port {
+    name = "httpsPort"
+    port = 443
+    }
+
+    frontend_ip_configuration {
+    name                 = "front_ip"
+    public_ip_address_id = azurerm_public_ip.agw-ip.id #frontend ip
+    }
+
+    backend_address_pool {
+    name = "backend_pool"
+    }
+
+    backend_http_settings {
+    name                  = "http_setting"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 1
+    }
+
+    http_listener {
+    name                           = "listener"
+    frontend_ip_configuration_name = "front_ip"
+    frontend_port_name             = "httpPort"
+    protocol                       = "Http"
+    }
+
+    request_routing_rule {
+    name                       = "request_routing_rule"
+    rule_type                  = "Basic"
+    http_listener_name         = "listener"
+    backend_address_pool_name  = "backend_pool"
+    backend_http_settings_name = "http_setting"
+    }
+
+    depends_on = [azurerm_public_ip.agw-ip]
+}
