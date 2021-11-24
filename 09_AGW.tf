a89e5ca1047782edfe1005955f2c9e4b27ab56f6
HemMu <kimmim1223@naver.com>
2021-11-24 17:20:09 +0900

    11-24-17:20

--- 09_AGW.tf
+++ 09_AGW.tf
@@ -47,48 +47,27 @@ resource "azurerm_application_gateway" "agw" {
     port                  = 80
     protocol              = "Http"
     request_timeout       = 1
-    }
-    backend_http_settings {
-    name                  = "https_setting"
-    cookie_based_affinity = "Disabled"
-    port                  = 443
-    protocol              = "Https"
-    request_timeout       = 1
-    }
+    } # Http
 
     http_listener {
     name                           = "listener"
     frontend_ip_configuration_name = "front_ip"
     frontend_port_name             = "httpPort"
     protocol                       = "Http"
-    }
-    http_listener {
-    name                           = "ssl"
-    frontend_ip_configuration_name = "front_ip"
-    frontend_port_name             = "httpsPort"
-    protocol                       = "Https"
-    ssl_certificate_name = "hamcert"
-    }
-    ssl_certificate{
-        name = "hamcert"
-        data = filebase64("./certificate.pfx")
-        password = "it1"
-    }
-
+    } # Http
+    
     request_routing_rule {
     name                       = "http_routing_rule"
     rule_type                  = "Basic"
     http_listener_name         = "listener"
     backend_address_pool_name  = "backend_pool"
     backend_http_settings_name = "http_setting"
-    }
-    request_routing_rule {
-    name                       = "https_routing_rule"
-    rule_type                  = "Basic"
-    http_listener_name         = "ssl"
-    backend_address_pool_name  = "backend_pool"
-    backend_http_settings_name = "https_setting"
-    }
+    } # Http
 
+    ssl_certificate{
+        name = "hamcert"
+        data = filebase64("./certificate.pfx")
+        password = "it1"
+    } # ssl add
     depends_on = [azurerm_public_ip.agw-ip]
 }
