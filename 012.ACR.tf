02e16219d169000aa5540b920a8a9b9bd2a21671
HemMu <kimmim1223@naver.com>
2021-11-29 16:02:07 +0900

    11-29-16:02

--- 012.ACR.tf
+++ 012.ACR.tf
@@ -6,3 +6,8 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 # az aks update -n terraform-aks -g hem-resources --attach-acr hemgistry
+resource "azurerm_role_assignment" "ra5" {
+    scope                = azurerm_container_registry.acr.id
+    role_definition_name = "AcrPull"
+    principal_id         = var.aks_service_principal_object_id
+}
